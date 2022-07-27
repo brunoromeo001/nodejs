5f017985b09550b47e74c65ccae3b92936e155cf
Bruno Romeo <brunogfvot@hotmail.com>
2022-07-27 05:49:07 -0300

    aula 36

---
+++ secao-05/module-01.js
@@ -0,0 +1,11 @@
+console.log("Executando module-01");
+
+const oculta = ()=>{
+
+  console.log("Executando função oculta");
+}
+
+module.exports.executa = ()=>{
+  
+  console.log("Executando a função oculta");
+}
