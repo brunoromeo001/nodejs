e1588e6fe9259f5fddfbe0b9bd8e1a9c33953e9e
Bruno Romeo <brunogfvot@hotmail.com>
2022-07-28 05:12:27 -0300

    aula 57

--- secao-08/app.js
+++ secao-08/app.js
@@ -5,6 +5,15 @@ const usuarioRoutes = require("./routes/usuario")
 
 app.use(express.json())
 
+app.use((req, res, next)=>{
+
+  console.log("Execultando o Middleware")
+
+  return next()
+})
+
+
+
 app.get('/', (req, res)=>{
   
   res.send("Servidor OK")
--- secao-08/routes/usuario.js
+++ secao-08/routes/usuario.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res)=>{
+function logReq(req, res, next){
+
+  console.log("Executando a função Middleware para rota usuários")
+  return next()
+}
+
+router.get('/', logReq,(req, res)=>{
   
   res.send("Listando usuarios")
 })
 
-router.get('/:id', (req, res)=>{
+router.get('/:id',  (req, res)=>{
   
   res.send("Listando usuario com id: "+ req.params.id)
 })
