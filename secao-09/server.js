adbf8482c69755782e451a2f5fde5bd2df0451bf
Bruno Romeo <brunogfvot@hotmail.com>
2022-07-29 00:37:25 -0300

    aula 63

--- secao-09/server.js
+++ secao-09/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParse = require('body-parser')
 const multer = require("multer")
 const app = express()
+const path = require("path")
 
 app.use(bodyParse.urlencoded({extended:true}))
 
@@ -11,6 +12,17 @@ app.get("/", (req, res)=>{
 
   res.sendFile(__dirname+'/index.html')
 })
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb)=>{
+    cb(null, file.fieldname+'-'+Date.now()+path.extname(file.originalname))
+  }
+})
+
+const upload = multer({storage})
 
 app.listen(3000, '127.0.0.1', ()=>{
 
