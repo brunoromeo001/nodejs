97f8476d3868590a43b481e60a53700d7143fc70
Bruno Romeo <brunogfvot@hotmail.com>
2022-07-29 00:30:26 -0300

    aula 62

--- secao-09/serve.js
+++ secao-09/serve.js
@@ -7,7 +7,9 @@ app.use(bodyParse.urlencoded({extended:true}))
 
 app.get("/", (req, res)=>{
 
-  res.json({message: "Bem-vindo"})
+  //res.json({message: "Bem-vindo"})
+
+  res.sendFile(__dirname+'/index.html')
 })
 
 app.listen(3000, '127.0.0.1', ()=>{
