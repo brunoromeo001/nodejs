f5c1d0c979d0d4e805a8f1a3872cacc1cd0b0aab
Bruno Romeo <brunogfvot@hotmail.com>
2022-07-29 03:33:45 -0300

    aula 76

--- secao-11/server.js
+++ secao-11/server.js
@@ -1,5 +1,4 @@
 require("dotenv").config()
-
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
@@ -11,5 +10,8 @@ db.on('error', (err)=> console.log(err))
 db.once('open', ()=> console.log("Database Connected"))
 
 app.use(express.json)
+
+const subscribersRouter = require("./routes/subscribers")
+app.use("/subscribers", subscribersRouter)
 
 app.listen(3000, ()=> console.log("Server running..."))
