83ec25e9d3179206ea7b90c128aa7566aa0b8864
Bruno Romeo <brunogfvot@hotmail.com>
2022-07-29 00:22:45 -0300

    aula 61

---
+++ secao-09/serve.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+const multer = require("multer")
+const app = express()
+
+app.use(bodyParse.urlencoded({extended:true}))
+
+app.get("/", (req, res)=>{
+
+  res.json({message: "Bem-vindo"})
+})
+
+app.listen(3000, '127.0.0.1', ()=>{
+
+  console.log("Server runnig at port 3000")
+})
